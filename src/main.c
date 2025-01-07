0e6b9686410a401f79445b28a632f68e213bf163
mahanfr <mahan.farzaneh@gmail.com>
2025-01-07 23:19:24 +0330

    Checking the linux version

--- src/main.c
+++ src/main.c
@@ -1,11 +1,13 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/mount.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
+#include <sys/utsname.h>
 #include <linux/sched.h>
 #include <sched.h>
 #include <sys/syscall.h>
@@ -26,11 +28,35 @@ void call_usage(char* program_name) {
     exit(EXIT_FAILURE);
 }
 
-int choose_hostname(char *buf, size_t len) {
+void generate_hostname(char *buf, size_t len) {
     struct timespec now = {0};
     clock_gettime(CLOCK_MONOTONIC, &now);
     size_t ix = now.tv_nsec;
     snprintf(buf, len, "%05lx-%s", now.tv_sec, "container");
+}
+
+int check_linux_version() {
+    fprintf(stdout, "validating Linux version...");
+    struct utsname host = {0};
+    if (uname(&host) != 0) {
+        fprintf(stderr, "failed: %m\n");
+        return 1;
+    }
+    int major = -1;
+    int minor = -1;
+    if (sscanf(host.release, "%u.%u", &major, &minor) != 2) {
+        fprintf(stderr, "unexpected relase format %s\n", host.release);
+        return 1;
+    }
+    if (major < 4) {
+        fprintf(stderr, "expected kernerl version >4.7.x: %s\n",host.release);
+        return 1;
+    }
+    if (strcmp("x86_64", host.machine)) {
+        fprintf(stderr, "expected arch x86_64: %s\n",host.machine);
+        return 1;
+    }
+    fprintf(stdout, "%s on %s.\n", host.release, host.machine);
     return 0;
 }
 
@@ -62,9 +88,13 @@ int main(int argc, char **argv) {
     if (!config.mount_dir) call_usage(argv[0]);
 
     char hostname[256] = {0};
-    choose_hostname(hostname, sizeof(hostname));
+    generate_hostname(hostname, sizeof(hostname));
     config.hostname = hostname;
     printf("%s \n", config.hostname);
+
+    (void) check_linux_version();
+
+    // NameSpace
 
     return 0;
 }
