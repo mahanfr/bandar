191ea5157a7cd55771fa1b674a23481bcd9610c7
mahanfr <mahan.farzaneh@gmail.com>
2025-01-09 15:14:45 +0330

    Disabling dangerous syscalls

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-#include <linux/prctl.h>
 #define _GNU_SOURCE
 #include <sys/types.h>
 #include <stdio.h>
@@ -7,6 +6,8 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <seccomp.h>
+#include <errno.h>
 #include <sys/mount.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -239,6 +240,52 @@ int mounts(struct child_config *config) {
     }
     fprintf(stderr, "done.\n");
     return 0;
+}
+
+#define SCMP_FAIL SCMP_ACT_ERRNO(EPERM)
+
+int disable_syscalls() {
+    scmp_filter_ctx ctx = NULL;
+    fprintf(stderr, "Filtering Syscalls...");
+    if (!(ctx = seccomp_init(SCMP_ACT_ALLOW))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1,
+            SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1,
+            SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1,
+            SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1,
+            SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1,
+            SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1,
+            SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(unshare), 1,
+            SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(clone), 1,
+            SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ioctl), 1,
+            SCMP_A1(SCMP_CMP_MASKED_EQ, TIOCSTI, TIOCSTI))
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(keyctl), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(add_key), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(request_key), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ptrace), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(mbind), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(migrate_pages), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(move_pages), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(set_mempolicy), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(userfaultfd), 0)
+        || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(perf_event_open), 0)
+        || seccomp_attr_set(ctx, SCMP_FLTATR_CTL_NNP, 0)
+        || seccomp_load(ctx))
+    {
+        if (ctx) seccomp_release(ctx);
+        fprintf(stderr, "failed: %m\n");
+        return 1;
+    }
+    seccomp_release(ctx);
+	fprintf(stderr, "done.\n");
+	return 0;
 }
 
 int child(void *arg) {
